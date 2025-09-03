cfe94035dae16c844a2c20165ef319dc61adc1f1
Fahim <you@example.com>
2025-09-03 11:09:14 -0700

    File moved

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import http from "http";
+import buildApp from "./app";
+import { connectDB } from "./config/db";
+import { initSocket } from "./socket";
+
+const PORT = Number(process.env.PORT || 3000);
+
+async function main() {
+  await connectDB(process.env.MONGO_URI!);
+
+  const server = http.createServer();
+  const ioHelpers = initSocket(server);
+
+  const app = buildApp(ioHelpers);
+  server.on("request", app);
+
+  server.listen(PORT, () => console.log(`🚀 API + WS on http://localhost:${PORT}`));
+}
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
