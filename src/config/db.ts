1f772206cfca59f5f02023c359f5da9e390d48e1
nhumayun2 <nhumayun291@gmail.com>
2025-09-30 11:27:18 +0600

    added .env

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 
-export async function connectDB(uri: string) {
+export async function connectDB(p0: string) {
+  const uri = process.env.MONGO_URI;
+  if (!uri) {
+    throw new Error("❌ MONGO_URI is not defined in .env");
+  }
+
   if (mongoose.connection.readyState === 1) return;
+
   mongoose.set("strictQuery", true);
   await mongoose.connect(uri);
   console.log("✅ Mongo connected");
